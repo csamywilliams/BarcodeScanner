6a326e5b94998cf08c6b649ce9a718ce94244181
Amy Jenkins <amyjenkins@Amys-MacBook-Pro.local>
2016-06-11 12:10:59 +0100

    Added validation to barcode action

--- js/Csaw/BarcodeScanner/trigger.js
+++ js/Csaw/BarcodeScanner/trigger.js
@@ -1,5 +1,34 @@
 jQuery(function($){
   $( document ).ready(function(){
-    console.log("im ready");
+    $('#btn-start').click(function(){
+      var action = $('#action-choice').val();
+
+      var identifier = [];
+      $.each($("input[name='identifier[]']:checked"), function() {
+        identifier.push($(this).val());
+      });
+
+      var validated = validateActions(action, identifier);
+      console.log(validated);
+
+    });
+
   });
 });
+
+/**
+* function validateActions, the action for stock movement cannot be blank nor
+* can the identifiers it must be able to match on ID, SKU or barcode
+* @param action must have an incoming or outgoing action
+* @param identifiers must be able to match on ID, SKU or barcode chosen by user
+* @return if validation is successful or not
+*/
+function validateActions(action,identifiers)
+{
+  var validated = false;
+  if(action != "blank" && identifiers.length != 0)
+  {
+    validated = true;
+  }
+  return validated;
+}
