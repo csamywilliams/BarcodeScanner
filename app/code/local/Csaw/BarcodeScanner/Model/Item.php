479e45d529f8617e8725fa967b7db1d74e78395b
Amy Williams <csamywilliams@gmail.com>
2016-06-21 14:35:39 +0100

    Added stock required attribute

--- app/code/local/Csaw/BarcodeScanner/Model/Item.php
+++ app/code/local/Csaw/BarcodeScanner/Model/Item.php
@@ -15,6 +15,7 @@ class Csaw_BarcodeScanner_Model_Item
      $this->id = $data['product']['entity_id'];
      $this->sku = $data['product']['sku'];
      $this->qty = (int) $data['stock'];
+     $this->stock_required = $data['product']['stock_required'];
      $this->name = $data['product']['name'];
     // $this->stock_required = $stock_required;
      $this->gtin = $data['product']['c2c_gtin'];
